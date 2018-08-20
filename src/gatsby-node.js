6f99ee6155e999639150483b71b24785a1c2212a
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2018-08-19 17:35:40 -1000

    Add basic implementation

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,7 +1,55 @@
+import createNodeHelpers from 'gatsby-node-helpers'
 import lunr from 'lunr'
+import R from 'ramda'
+import { map } from 'p-iteration'
 
-export const onPostBuild = ({ graphql }, { queries }) => {
-  const jobs = queries.map(async query => {})
+const { createNodeFactory } = createNodeHelpers({ typePrefix: 'SearchIndex' })
 
-  return Promise.all(jobs)
+/***
+ * Takes a query object with accessory options and returns a node to be passed
+ * to `createNode`.
+ */
+const prepareNode = graphql => async ({
+  name,
+  ref = 'id',
+  query,
+  normalizer,
+}) => {
+  const documents = await R.pipeP(
+    graphql,
+    normalizer,
+  )(query)
+
+  if (R.empty(documents)) return
+
+  const fields = R.pipe(
+    R.head,
+    R.keys,
+    R.reject(R.equals(ref)),
+  )(documents)
+
+  const index = lunr(function() {
+    this.ref(ref)
+    fields.forEach(x => this.field(x))
+    documents.forEach(x => this.add(x))
+  })
+
+  const SearchIndexNode = createNodeFactory(name)
+
+  return SearchIndexNode({
+    id: name,
+    index: JSON.stringify(index),
+  })
 }
+
+export const createPages = async (
+  { graphql, actions: { createNode } },
+  { queries = [] },
+) =>
+  map(
+    queries,
+    R.pipeP(
+      prepareNode(graphql),
+      createNode,
+    ),
+  )
