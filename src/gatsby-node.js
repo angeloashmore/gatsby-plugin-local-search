3e4ffdc003f871dab5c800ad1aacade10144322e
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2018-08-24 13:28:33 -1000

    Support only one query per plugin instance

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -5,22 +5,20 @@ import R from 'ramda'
 
 const { createNodeFactory } = createNodeHelpers({ typePrefix: 'LocalSearch' })
 
-/***
- * Takes a query object with accessory options and returns an object to be
- * passed to `createNode`.
- */
-const prepareNode = graphql => async ({
-  name,
-  ref = 'id',
-  query,
-  normalizer,
-  store: storeFields = [],
-}) => {
+export const createPages = async (
+  { graphql, actions: { createNode } },
+  { name, ref = 'id', store: storeFields, query, normalizer },
+) => {
   const result = await graphql(query)
   if (result.errors) throw R.head(result.errors)
 
   const documents = await Promise.resolve(normalizer(result))
-  if (R.isEmpty(documents)) return
+  if (R.isEmpty(documents)) {
+    console.log(
+      `gatsby-plugin-local-search returned no documents for query "${name}"`,
+    )
+    return
+  }
 
   const fields = R.pipe(
     R.head,
@@ -39,23 +37,14 @@ const prepareNode = graphql => async ({
     R.indexBy(R.prop(ref)),
   )(documents)
 
-  const LocalSearchNode = createNodeFactory(name)
-
-  return LocalSearchNode({
+  R.pipe(
+    createNodeFactory(name),
+    createNode,
+  )({
     id: name,
     index: JSON.stringify(index),
     store: JSON.stringify(store),
   })
-}
 
-export const createPages = async (
-  { graphql, actions: { createNode } },
-  { queries = [] },
-) =>
-  forEach(
-    queries,
-    R.pipeP(
-      prepareNode(graphql),
-      createNode,
-    ),
-  )
+  return
+}
